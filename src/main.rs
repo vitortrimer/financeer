82961864a81626665787e686f5a980ff5aeb64fb
Vitor Trimer <vitortrimer@gmail.com>
2024-01-20 18:10:59 -0300

    Setup healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Hellow");
+use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
+
+#[get("/api/healthcheck")]
+async fn health_check_handler() -> impl Responder {
+    const MESSAGE: &str = "Financeer web services is running";
+
+    HttpResponse::Ok().json(serde_json::json!({"status": "success", "message": MESSAGE}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+
+    env_logger::init();
+    println!("🚀 Server started on port 8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(health_check_handler)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
